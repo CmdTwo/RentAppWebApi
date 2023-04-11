2d369b964741805b147e445b4051a591009de1b7
Alexey <coommandoos@gmail.com>
2023-04-11 18:51:42 +0200

    Updated migration

--- RentAppWebApi/Init/Seed.cs
+++ RentAppWebApi/Init/Seed.cs
@@ -17,26 +17,34 @@ namespace RentAppWebApi.Init
             if (!dataContext.Users.Any())
             {
                 var users = new List<User>()
-                {
-                    new User()
                     {
-                        Login = "admin",
-                        Password = "admin",
-                        Name = "TestUser_1",
-                        Surname = "SomeSurmame",
-                        PhotoPath = "photos/defalut.jpg",
-                        Reviews = new List<Review>()
-                    },
-                    new User()
-                    {
-                        Login = "test",
-                        Password = "test",
-                        Name = "TestUser_2",
-                        Surname = "SomeSurmame_2",
-                        PhotoPath = "photos/defalut.jpg",
-                        Reviews = new List<Review>()
-                    }
-                };                
+                        new User()
+                        {
+                            Login = "admin",
+                            Password = "admin",
+                            Name = "TestUser_1",
+                            Surname = "SomeSurmame",
+                            PhotoPath = "photos/defalut.jpg",
+                            Reviews = new List<Review>(),
+                            Advertisements = new List<Advertisement>(),
+                            ContractsAsLandLord = new List<Contract>(),
+                            ContractsAsRenter = new List<Contract>(),
+                            RealEstates = new List<RealEstate>()
+                        },
+                        new User()
+                        {
+                            Login = "test",
+                            Password = "test",
+                            Name = "TestUser_2",
+                            Surname = "SomeSurmame_2",
+                            PhotoPath = "photos/defalut.jpg",
+                            Reviews = new List<Review>(),
+                            Advertisements = new List<Advertisement>(),
+                            ContractsAsLandLord = new List<Contract>(),
+                            ContractsAsRenter = new List<Contract>(),
+                            RealEstates = new List<RealEstate>()
+                        }
+                    };
 
                 var country = new Country()
                 {
@@ -44,22 +52,13 @@ namespace RentAppWebApi.Init
                 };
 
                 var cities = new List<City>()
-                {
-                    new City() { Name="Minsk", Country = country },
-                    new City() { Name="Brest", Country = country },
-                    new City() { Name="Pinsk", Country = country }
-                };
+                    {
+                        new City() { Name="Minsk", Country = country },
+                        new City() { Name="Brest", Country = country },
+                        new City() { Name="Pinsk", Country = country }
+                    };
 
                 country.Cities = cities;
-
-                var landLord = new LandLord()
-                {
-                    User = users[0],
-                    ReviewableObject = new ReviewableObject(),
-                    Contracts = new List<Contract>(),
-                    Advertisements = new List<Advertisement>(),
-                    RealEstates = new List<RealEstate>()
-                };
 
                 var realEstate = new RealEstate()
                 {
@@ -67,21 +66,21 @@ namespace RentAppWebApi.Init
                     Location = "Lenin st. 1 a",
                     Country = country,
                     City = cities[0],
-                    LandLord = landLord,
-                    ReviewableObject = new ReviewableObject(),
+                    LandLord = users[0],
                     Advertisements = new List<Advertisement>(),
-                    Contracts = new List<Contract>()
+                    Contracts = new List<Contract>(),
+                    Photos = new List<PhotoOfRealEstate>()
                 };
 
                 cities[0].RealEstates = new List<RealEstate>() { realEstate };
                 country.RealEstates = new List<RealEstate>() { realEstate };
 
                 var photos = new List<PhotoOfRealEstate>()
-                {
-                    new PhotoOfRealEstate() { ImagePath = "photos/default.jpg", RealEstate = realEstate },
-                    new PhotoOfRealEstate() { ImagePath = "photos/default.jpg", RealEstate = realEstate },
-                    new PhotoOfRealEstate() { ImagePath = "photos/default.jpg", RealEstate = realEstate }
-                };
+                    {
+                        new PhotoOfRealEstate() { ImagePath = "photos/default.jpg", RealEstate = realEstate },
+                        new PhotoOfRealEstate() { ImagePath = "photos/default.jpg", RealEstate = realEstate },
+                        new PhotoOfRealEstate() { ImagePath = "photos/default.jpg", RealEstate = realEstate }
+                    };
 
                 var advertisement = new Advertisement()
                 {
@@ -90,15 +89,8 @@ namespace RentAppWebApi.Init
                     Date = DateTime.Now,
                     TermInMonths = 6,
                     IsActual = true,
-                    LandLord = landLord,
+                    LandLord = users[0],
                     RealEstate = realEstate
-                };
-
-                var renter = new Renter()
-                {
-                    User = users[1],
-                    ReviewableObject = new ReviewableObject(),
-                    Contracts = new List<Contract>()
                 };
 
                 var contract = new Contract()
@@ -106,56 +98,49 @@ namespace RentAppWebApi.Init
                     Date = DateTime.Now,
                     TermInMonths = 6,
                     IsActual = true,
-                    LandLord = landLord,
-                    Renter = renter,
-                    RealEstate = realEstate
+                    RealEstate = realEstate,
+                    Reviews = new List<Review>(),
+                    LandLord = users[0],
+                    Renter = users[1]
                 };
 
                 realEstate.Photos = photos;
                 realEstate.Advertisements.Add(advertisement);
                 realEstate.Contracts.Add(contract);
 
-                landLord.Advertisements.Add(advertisement);
-                landLord.RealEstates.Add(realEstate);
-                landLord.Contracts.Add(contract);
-
-                renter.Contracts.Add(contract);
+                users[0].Advertisements.Add(advertisement);
+                users[0].RealEstates.Add(realEstate);
+                users[0].ContractsAsLandLord.Add(contract);
+                users[1].ContractsAsRenter.Add(contract);
 
                 var reviews = new List<Review>()
-                {
-                    new Review()
                     {
-                        Comment = "Good one",
-                        Rate = 5,
-                        User = users[1],
-                        ReviewableObjects = new List<ReviewableObject>() { realEstate.ReviewableObject }
-                    },
-                    new Review()
-                    {
-                        Comment = "Nice landlord!",
-                        Rate = 5,
-                        User = users[1],
-                        ReviewableObjects = new List<ReviewableObject>() { landLord.ReviewableObject }
-                    },
-                    new Review()
-                    {
-                        Comment = "Nice person",
-                        Rate = 5,
-                        User = users[0],
-                        ReviewableObjects = new List<ReviewableObject>() { renter.ReviewableObject }
-                    }
-                };
+                        new Review()
+                        {
+                            Comment = "Good one",
+                            Rate = 5,
+                            User = users[1],
+                            Contract = contract
+                        },
+                        new Review()
+                        {
+                            Comment = "Nice person",
+                            Rate = 5,
+                            User = users[0],
+                            Contract = contract
+                        }
+                    };
+
+                contract.Reviews.Add(reviews[0]);
+                contract.Reviews.Add(reviews[1]);
 
                 users[1].Reviews.Add(reviews[0]);
-                users[1].Reviews.Add(reviews[1]);
-                users[0].Reviews.Add(reviews[2]);
+                users[0].Reviews.Add(reviews[1]);
 
                 dataContext.Users.AddRange(users);
-                dataContext.LandLords.Add(landLord);
-                dataContext.Renters.Add(renter);
 
                 dataContext.SaveChanges();
             }
-        }        
+        }
     }
 }
---
+++ RentAppWebApi/Migrations/20230329130458_InitialCreate.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RentAppWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    PhotoPath = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CountryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RealEstates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    CountryId = table.Column<int>(type: "integer", nullable: false),
+                    CityId = table.Column<int>(type: "integer", nullable: false),
+                    LandLordId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RealEstates_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RealEstates_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RealEstates_Users_LandLordId",
+                        column: x => x.LandLordId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advertisements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Header = table.Column<string>(type: "text", nullable: false),
+                    Cost = table.Column<double>(type: "double precision", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    TermInMonths = table.Column<int>(type: "integer", nullable: false),
+                    IsActual = table.Column<bool>(type: "boolean", nullable: false),
+                    LandLordId = table.Column<int>(type: "integer", nullable: false),
+                    RealEstateId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advertisements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Advertisements_RealEstates_RealEstateId",
+                        column: x => x.RealEstateId,
+                        principalTable: "RealEstates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Advertisements_Users_LandLordId",
+                        column: x => x.LandLordId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contracts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    TermInMonths = table.Column<int>(type: "integer", nullable: false),
+                    IsActual = table.Column<bool>(type: "boolean", nullable: false),
+                    RealEstateId = table.Column<int>(type: "integer", nullable: false),
+                    LandLordId = table.Column<int>(type: "integer", nullable: false),
+                    RenterId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contracts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contracts_RealEstates_RealEstateId",
+                        column: x => x.RealEstateId,
+                        principalTable: "RealEstates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contracts_Users_LandLordId",
+                        column: x => x.LandLordId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contracts_Users_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhotoOfRealEstates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ImagePath = table.Column<string>(type: "text", nullable: false),
+                    RealEstateId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhotoOfRealEstates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PhotoOfRealEstates_RealEstates_RealEstateId",
+                        column: x => x.RealEstateId,
+                        principalTable: "RealEstates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Comment = table.Column<string>(type: "text", nullable: false),
+                    Rate = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ContractId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Contracts_ContractId",
+                        column: x => x.ContractId,
+                        principalTable: "Contracts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisements_LandLordId",
+                table: "Advertisements",
+                column: "LandLordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisements_RealEstateId",
+                table: "Advertisements",
+                column: "RealEstateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_LandLordId",
+                table: "Contracts",
+                column: "LandLordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_RealEstateId",
+                table: "Contracts",
+                column: "RealEstateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_RenterId",
+                table: "Contracts",
+                column: "RenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhotoOfRealEstates_RealEstateId",
+                table: "PhotoOfRealEstates",
+                column: "RealEstateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstates_CityId",
+                table: "RealEstates",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstates_CountryId",
+                table: "RealEstates",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstates_LandLordId",
+                table: "RealEstates",
+                column: "LandLordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ContractId",
+                table: "Reviews",
+                column: "ContractId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Advertisements");
+
+            migrationBuilder.DropTable(
+                name: "PhotoOfRealEstates");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Contracts");
+
+            migrationBuilder.DropTable(
+                name: "RealEstates");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
