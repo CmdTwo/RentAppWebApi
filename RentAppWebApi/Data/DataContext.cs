9dc891874cae7b5ebd24974ad82e474ba0344f6d
Alexey <coommandoos@gmail.com>
2023-04-11 18:53:14 +0200

    Updated datacontext

--- RentAppWebApi/Data/DataContext.cs
+++ RentAppWebApi/Data/DataContext.cs
@@ -14,17 +14,24 @@ namespace RentAppWebApi.Data
         public DbSet<City> Cities { get; set; }
         public DbSet<Contract> Contracts { get; set; }
         public DbSet<Country> Countries { get; set; }
-        public DbSet<LandLord> LandLords { get; set; }
         public DbSet<PhotoOfRealEstate> PhotoOfRealEstates { get; set; }
         public DbSet<RealEstate> RealEstates { get; set; }
-        public DbSet<Renter> Renters { get; set; }
         public DbSet<Review> Reviews { get; set; }
-        public DbSet<ReviewableObject> ReviewableObjects { get; set; }
         public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<Contract>()
+                .HasOne(contract => contract.LandLord)
+                .WithMany(x => x.ContractsAsLandLord)
+                .HasForeignKey(contract => contract.LandLordId)
+                .IsRequired();
+
+            modelBuilder.Entity<Contract>()
+                .HasOne(contract => contract.Renter)
+                .WithMany(x => x.ContractsAsRenter)
+                .HasForeignKey(contract => contract.RenterId)
+                .IsRequired();
         }
     }
 }
